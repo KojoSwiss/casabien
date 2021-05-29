4285325b58ebb048ba8c5baeea69a010b8009de4
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-05-29 15:09:13 +0200

    navbar added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :accounts
+  # devise_for :accounts
+  resources :properties
   root to: 'public#main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
